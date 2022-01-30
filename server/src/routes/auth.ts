017be16365c14fbc2603bb4c4d2f890598445dc4
hannahmanuela <hannahmngross@gmail.com>
2022-01-30 12:10:16 -0500

    setup working

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -7,9 +7,15 @@ const authRouter = Router();
 
 // auth apis will go here http://www.passportjs.org/docs/google/
 
+authRouter.get("/test", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
 
 // this all needs the config setup (see ../config/passport.ts) 
 
+
+export default authRouter;
 
 
 
