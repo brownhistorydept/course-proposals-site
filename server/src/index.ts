f8959127885fd6d74e15e6d7f3c9fb943b9b4945
hannahmanuela <hannahmngross@gmail.com>
2022-02-05 15:54:14 -0500

    after meeting

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,60 @@ import authRouter from "./routes/auth";
 
 export function main() {
 
-  const app = express();
+  // this all needs the config setup (see ../config/passport.ts) 
+
+  const express = require('express')
+  const app = express()
+
+  const session = require('express-session')
+  const passport = require('passport')
+
+  const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+
+  app.use(session({
+      secret: "secret",
+      resave: false ,
+      saveUninitialized: true ,
+  }))
+
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  // TODO: INSERT
+  const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+  const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+
+  function authUser(request, accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+
+  //Use "GoogleStrategy" as the Authentication Strategy
+  passport.use(new GoogleStrategy({
+      clientID:     GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3001/auth/google/callback",
+      passReqToCallback   : true
+    }, authUser));
+
+
+  passport.serializeUser( (user, done) => { 
+      console.log(`\n--------> Serialize User:`)
+      console.log(user)
+      // The USER object is the "authenticated user" from the done() in authUser function.
+      // serializeUser() will attach this user to "req.session.passport.user.{user}", so that it is tied to the session object for each session.  
+
+      done(null, user)
+  } )
+
+  passport.deserializeUser((user, done) => {
+          console.log("\n--------- Deserialized User:")
+          console.log(user)
+          // This is the {user} that was saved in req.session.passport.user.{user} in the serializationUser()
+          // deserializeUser will attach this {user} to the "req.user.{user}", so that it can be used anywhere in the App.
+
+          done (null, user)
+  }) 
+
 
   app.use("/auth", authRouter);
 
@@ -21,12 +74,12 @@ export function main() {
         methods: "GET,PUT,DELETE",
         credentials: true,
     })
-);
+  );
 
-// start server
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-});
+  // start server
+  app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+  });
 
 }
 
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -12,77 +12,21 @@ authRouter.get("/test", (req, res) => {
 });
 
 
-// this all needs the config setup (see ../config/passport.ts) 
-
-const express = require('express')
-const app = express()
-
-const session = require('express-session')
-const passport = require('passport')
-
-const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
-
-app.use(session({
-    secret: "secret",
-    resave: false ,
-    saveUninitialized: true ,
-}))
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-// TODO: INSERT
-const GOOGLE_CLIENT_ID = "108318053408-70egqlado3ahcqflcr87vqgl6g62qoda.apps.googleusercontent.com"
-const GOOGLE_CLIENT_SECRET = "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"
-
-function authUser(request, accessToken, refreshToken, profile, done) {
-    return done(null, profile);
-  }
-
-//Use "GoogleStrategy" as the Authentication Strategy
-passport.use(new GoogleStrategy({
-    clientID:     GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3001/auth/google/callback",
-    passReqToCallback   : true
-  }, authUser));
-
-
-passport.serializeUser( (user, done) => { 
-    console.log(`\n--------> Serialize User:`)
-    console.log(user)
-     // The USER object is the "authenticated user" from the done() in authUser function.
-     // serializeUser() will attach this user to "req.session.passport.user.{user}", so that it is tied to the session object for each session.  
-
-    done(null, user)
-} )
-
-passport.deserializeUser((user, done) => {
-        console.log("\n--------- Deserialized User:")
-        console.log(user)
-        // This is the {user} that was saved in req.session.passport.user.{user} in the serializationUser()
-        // deserializeUser will attach this {user} to the "req.user.{user}", so that it can be used anywhere in the App.
-
-        done (null, user)
-}) 
-
-
-app.listen(3001, () => console.log(`Server started on port 3001...`))
-
-app.get('/auth/google',
+authRouter.get('/auth/google',
   passport.authenticate('google', { scope:
       [ 'email', 'profile' ] }
 ));
 
-app.get('/auth/google/callback',
+authRouter.get('/auth/google/callback',
     passport.authenticate( 'google', {
         successRedirect: '/dashboard',
         failureRedirect: '/login'
 }));
 
 //Define the Login Route
-app.get("/login", (req, res) => {
-    res.render("../views/login.ejs")
+authRouter.get("/login", (req, res) => {
+    console.log(`-------> User logging in`)
+    res.render("./../public/views/login.ejs")
 })
 
 //Use the req.isAuthenticated() function to check if user is Authenticated
@@ -92,12 +36,12 @@ const checkAuthenticated = (req, res, next) => {
 }
 
 //Define the Protected Route, by using the "checkAuthenticated" function defined above as middleware
-app.get("/dashboard", checkAuthenticated, (req, res) => {
-  res.render("../views/dashboard.ejs", {name: req.user.displayName})
+authRouter.get("/dashboard", checkAuthenticated, (req, res) => {
+  res.render(__dirname + "../../views/dashboard.ejs", {name: req.user.displayName})
 })
 
 //Define the Logout
-app.post("/logout", (req,res) => {
+authRouter.post("/logout", (req,res) => {
     req.logOut()
     res.redirect("/login")
     console.log(`-------> User Logged out`)
