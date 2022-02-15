fb6f9ae782e5b8dd0caf00676d4df3b158805997
Loh Jian Cong <lohjiancong@hotmail.com>
2022-02-15 15:31:51 -0500

    Login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,17 @@ import React from "react";
 import "./App.css";
 import NavBar from './components/NavBar';
 import CourseProposal from './components/CourseProposal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+
+// const theme = createTheme({
+//   typography: {
+//     fontFamily: 'Roboto',
+//   }});
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -18,12 +26,41 @@ function App() {
 
   return (
     <div className="App">
+      {/* <ThemeProvider theme={theme}> */}
       <NavBar/>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
       </header> */}
       {/* <CourseProposal/> */}
+      {/* { <h1>Welcome!</h1> }
+      { <h4>To submit or review History CourseProposal proposals, log in with your Brown email</h4>}     */}
+        <Box       
+        sx={{
+        width: 500,
+        height: 300,
+        margin: 'auto',
+      }}
+      >
+      <Typography
+            variant="h3"
+            align ="left"
+            mt={10}
+          >
+            Welcome!
+          </Typography>
+
+          <Typography
+            variant="h5"
+            align ="left"
+          >
+            To submit or review History course proposals, log in with your Brown email.
+          </Typography>
+
+        </Box>
+        {/* </ThemeProvider> */}
+      
+      
     </div>
   );
 }
