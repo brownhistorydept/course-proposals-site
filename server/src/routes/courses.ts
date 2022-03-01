15356ee02b3ca2fae24390c3ba646c9f111a06c5
Carolyn Zech <carolynzech@gmail.com>
2022-03-01 13:52:08 -0500

    more endpoints

--- server/src/routes/courses.ts
+++ server/src/routes/courses.ts
@@ -1,15 +1,12 @@
 import { Request, Response, Router } from "express";
-import User, { IUser } from "../models/User";
 import Course, { ICourse } from "../models/Course"
-import { authCheck, IGetUserAuthInfoRequest } from "../middleware/auth";
+import { IGetUserAuthInfoRequest } from "../middleware/auth";
 import { get_permissions } from "../models/Permissions";
-import { updateLanguageServiceSourceFile } from "typescript";
 
 const courseRouter = Router();
 
 // get all courses
 courseRouter.get("/all", async (req: Request, res: Response) => {
-    // might have to use populate to get prof information from users table
     
     try {
         const results = await Course.find({}).populate('professors');
@@ -25,52 +22,53 @@ courseRouter.get("/all", async (req: Request, res: Response) => {
 );
 
 // get a specific course by its name
-// courseRouter.get("/name", async (req: Request, res: Response) => {
+courseRouter.get("/title", async (req: Request, res: Response) => {
 
-//     try {
-//         const results = await Course.find({course_name: req.body.course_name});
-//         res.status(200).json({results});
-//         console.log(results);
-//     } catch (err) {
-//         res.status(401).json({
-//             message: "no course found with this name",
-//         });
-//     }
-// }
-// );
+    try {
+        const results = await Course.find({course_title: req.body.course_title}).populate('professors');
+        res.status(200).json({results});
+        console.log(results);
+    } catch (err) {
+        console.log(err);
+        res.status(401).json({
+            message: "no course found with this name",
+        });
+    }
+}
+);
 
-// // create a course
-// courseRouter.post("/create", async (req: IGetUserAuthInfoRequest, res: Response) => {
+// create a course
+courseRouter.post("/create", async (req: IGetUserAuthInfoRequest, res: Response) => {
 
-//     const permissions = get_permissions(req.user.role);
+    const permissions = get_permissions(req.user.role);
 
-//     if (permissions.can_submit_courses) {
+    if (permissions.can_submit_courses) {
   
-//         const newCourse: ICourse = {
-//             course_number: req.body.course_number,
-//             course_title: req.body.course_title,
-//             semester: req.body.semester,
-//             final_time: req.body.final_time,
-//             time_ranking: req.body.time_ranking,
-//             professor: professors,
-//             is_DIAP: req.body.is_DIAP,
-//             is_WRIT: req.body.is_WRIT,
-//             is_Premodern: req.body.is_Premodern,
-//             course_type: req.body.course_type,
-//             geography: req.body.geography,
-//             is_remote: req.body.is_remote,
-//             is_intro: req.body.is_intro,
-//             description: req.body.description,
-//             further_notes: req.body.further_notes
-//         };
-//         const course = await Course.create(newCourse);
-//         console.log(course);
-//     } else {
-//         res.status(401).json({
-//             message: "user does not have permission to create a course",
-//         });
-//     }
-// }
-// );
+        const newCourse: ICourse = {
+            course_number: req.body.course_number,
+            course_title: req.body.course_title,
+            semester: req.body.semester,
+            final_time: req.body.final_time,
+            time_ranking: req.body.time_ranking,
+            professors: req.body.professor_ids,
+            is_DIAP: req.body.is_DIAP,
+            is_WRIT: req.body.is_WRIT,
+            is_Premodern: req.body.is_Premodern,
+            course_type: req.body.course_type,
+            geography: req.body.geography,
+            is_remote: req.body.is_remote,
+            is_intro: req.body.is_intro,
+            description: req.body.description,
+            further_notes: req.body.further_notes
+        };
+        const course = await Course.create(newCourse);
+        console.log(course);
+    } else {
+        res.status(401).json({
+            message: "user does not have permission to create a course",
+        });
+    }
+}
+);
 
 export default courseRouter;
---
+++ server/src/routes/users.ts
@@ -0,0 +1,22 @@
+import { Request, Response, Router } from "express";
+import User from "../models/User";
+
+const userRouter = Router();
+
+// get all users
+
+// get all courses
+userRouter.get("/all", async (req: Request, res: Response) => {
+    
+    try {
+        const results = await User.find({});
+        res.status(200).json({results});
+        console.log(results);
+    } catch (err) {
+        console.log(err);
+        res.status(401).json({
+            message: "getting users failed",
+        });
+    }
+}
+);
